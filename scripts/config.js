01c448d503cf5912871a8713be30eeac02bfeb27
January J. <galore.module5h@icloud.com>
2025-10-31 15:57:34 +0400

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,6 @@
 // scripts/config.js
 window.__MF_CONFIG = {
   API_BASE_URL: "https://maltese-first-capital-deluxe-backend.onrender.com",
-  TURNSTILE_SITE_KEY: "0x4AAAAAAB94rdqCX5koZxG-"
+  TURNSTILE_SITE_KEY: "0x4AAAAAAB94rdqCX5koZxG-",
+  __v: 2
 };
