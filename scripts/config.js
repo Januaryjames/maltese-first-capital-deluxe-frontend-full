465673411002411c25bcb5116b3c813bba31d945
January J. <galore.module5h@icloud.com>
2025-11-02 14:24:09 +0400

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -1,7 +1,5 @@
-<!-- /scripts/config.js -->
 <script>
   window.__MFC_CONFIG = {
-    API_BASE_URL: 'https://maltese-first-capital-deluxe-backend.onrender.com',
-    AUTH_LOGIN_PATH: '/api/auth/login'
+    API_BASE_URL: 'https://maltese-first-capital-deluxe-backend.onrender.com'
   };
 </script>
